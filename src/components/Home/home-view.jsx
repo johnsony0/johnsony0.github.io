bae9f1452b885bd676b0d3181878587b1dc6475f
Johnsony0 <johnsony@bu.edu>
2024-05-26 14:56:40 -0400

    added about me page

--- src/components/Home/home-view.jsx
+++ src/components/Home/home-view.jsx
@@ -3,9 +3,37 @@ import { Typography, useMediaQuery } from '@mui/material';
 import portrait from '../../assets/self-portrait.jpg';
 import { useTheme } from '@mui/material/styles';
 
+const Typewriter = ({ text, delay, theme }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    setCurrentText('');
+    setCurrentIndex(0);
+  }, [text]);
+
+  useEffect(() => {
+    let timeout;
+    if (currentIndex < text.length) {
+      timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+    }
+    
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, text]);
+
+  return (
+    <Typography variant="h4" sx={{ color: theme.palette.text.secondary }}>
+      {currentText}
+    </Typography>
+  );
+};
+
 function Home() {
-  const wordList = ['Software Designer','R&B Listener', 'Avid Gamer','Engineer'];
-  const helloList = ['Hi','你好','مرحبا','Hello','Bonjour','Привет','Hola']
+  const wordList = ['Software Designer', 'R&B Listener', 'Avid Gamer', 'Computer Engineer'];
+  const helloList = ['Hi', '你好', 'مرحبا', 'Hello', 'Bonjour', 'Привет', 'Hola'];
   const [wordIndex, setWordIndex] = useState(0);
   const [helloIndex, setHelloIndex] = useState(0);
   const theme = useTheme();
@@ -14,30 +42,25 @@ function Home() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setWordIndex(prevIndex => (prevIndex + 1) % wordList.length);
-      setHelloIndex(Math.floor(Math.random() * helloList.length))
-    }, 1000);
+      setHelloIndex(Math.floor(Math.random() * helloList.length));
+    }, 3000);
 
     return () => clearInterval(intervalId);
-  }, [wordList.length,helloList.length]);
+  }, [wordList.length, helloList.length]);
 
-  const imageClass = isMd ? 'background-image-md-sm' : 'background-image-lg'
-  const containerClass = isMd? 'container-md-sm' : 'container-lg'
-  const textOverlayClass = isMd ? 'text-overlay-md-sm' : 'text-overlay-lg'
-  const textOverlaybottom = isMd ? '30%' : '40%'
+  const textOverlayBottom = isMd ? '30%' : '50%';
 
   return (
-    <div className={containerClass}>
-      <img src={portrait} alt="Background" className={imageClass} />
-      <div className={textOverlayClass}>
+    <div className='container'>
+      <img src={portrait} alt="Background" className='background-image' />
+      <div className='text-overlay'>
         <Typography variant="h4" sx={{ color: theme.palette.text.secondary }}>
           <b>{helloList[helloIndex]}</b>
         </Typography>
-        <Typography variant="h4" sx={{ color: theme.palette.text.secondary, marginBottom:textOverlaybottom }}>
+        <Typography variant="h4" sx={{ color: theme.palette.text.secondary, marginBottom: textOverlayBottom }}>
           I'm <b>Johnson</b>
         </Typography>
-        <Typography variant="h4" sx={{ color: theme.palette.text.secondary }}>
-          {wordList[wordIndex]}
-        </Typography>
+        <Typewriter text={wordList[wordIndex]} delay={100} theme={theme} />
       </div>
     </div>
   );
