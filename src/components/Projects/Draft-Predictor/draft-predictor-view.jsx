8498194536e38ecb63efad8c04f2d84105fb243d
Johnsony0 <johnsony@bu.edu>
2024-07-30 03:02:10 -0400

    dropdowns work, encoding works

--- src/components/Projects/Draft-Predictor/draft-predictor-view.jsx
+++ src/components/Projects/Draft-Predictor/draft-predictor-view.jsx
@@ -1,45 +1,129 @@
-import React, { useEffect } from 'react';
+import React, {useState} from 'react';
 import * as ort from 'onnxruntime-web';
+import { Autocomplete, TextField, Grid, Button, Box } from '@mui/material';
+import { champions, regions, elos, game_modes, versions, encodeTeam } from './DraftData';
 
+//add region, elo, gamemode, patch, threshold
 function DraftPredictior(){
   ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
-    useEffect(() => {
-        const runModel = async () => {
-          const model_path = `${process.env.PUBLIC_URL}/models/NA1_ARAM_ANY_14-13_nn_model.onnx`
-          const session = await ort.InferenceSession.create(model_path);
-          const data = Float32Array.from([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 1., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
-          const tensor_data = new ort.Tensor('float32',data,[1,2,168])
-          const feeds = {input: tensor_data}
-          const results = await session.run(feeds)
-          console.log(results.output.data)
-        }
-        runModel();
-      }, []);
+  const [formData, setFormData] = useState({
+    blue_team: ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar'],
+    red_team: ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar'],
+    region: regions[0].label,
+    game_mode: game_modes[0].label,
+    elo: elos[0].label,
+    version: versions[0].label
+  });
 
+  const handleParameterChange = (parameter,newValue) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [parameter] : newValue
+    }));
+  }
+
+  const handleTeamChange = (team, index, newValue) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [team]: prevFormData[team].map((item, i) => (i === index ? newValue || '' : item))
+    }));
+  };
+
+  const runModel = async (e) => {
+    e.preventDefault()
+
+    const blue_team = encodeTeam(formData.blue_team)
+    const red_team = encodeTeam(formData.red_team)
+
+    const model_path = `${process.env.PUBLIC_URL}/models/NA1_ARAM_ANY_14-13_nn_model.onnx`
+    const session = await ort.InferenceSession.create(model_path);
+
+    const data = Float32Array.from([...blue_team,...red_team])
+    const tensor_data = new ort.Tensor('float32',data,[1,2,168])
+    const feeds = {input: tensor_data}
+
+    const results = await session.run(feeds)
+    console.log(results.output.data)
+  }
+
+  const createAutocomplete = (label,value,options,index) => {
     return(
-        <div>
-            <h1>Model Inference Result</h1>
-        </div>
+      <Grid item xs={3}>
+        <Autocomplete
+          value={value}
+          onChange={(event, newValue) => handleParameterChange(index, newValue)}
+          options={options.map(option => option.label)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={label}
+              variant="outlined"
+              placeholder={`Select ${label}`}
+              sx={{ marginTop: 2 }}
+            />
+          )}
+        />
+      </Grid>
     )
+  }
+
+  return (
+    <Box>
+      <form onSubmit={runModel}>
+      <Grid container spacing={2} sx={{marginBottom: 2, px:2}}>
+        {createAutocomplete('Region', formData.region, regions, handleParameterChange, 'region')}
+        {createAutocomplete('Game Modes', formData.game_mode, game_modes, handleParameterChange, 'game_mode')}
+        {createAutocomplete('Rank/Elo', formData.elo, elos, handleParameterChange, 'elo')}
+        {createAutocomplete('Version', formData.version, versions, handleParameterChange, 'version')}
+        <Grid item xs={6}>
+          {formData.blue_team.map((value, index) => (
+            <Autocomplete
+              fullWidth
+              key={`blue_team_${index + 1}`}
+              value={value}
+              onChange={(event, newValue) => handleTeamChange('blue_team', index, newValue)}
+              options={champions.map(champion => champion.label)}
+              isOptionEqualToValue={(option, value) => (value === '' || option === value)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  placeholder="Select Champion"
+                  sx={{ marginBottom: 1 }}
+                />
+              )}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={6}>
+          {formData.red_team.map((value, index) => (
+            <Autocomplete
+              fullWidth
+              key={`red_team_${index + 1}`}
+              value={value}
+              onChange={(event, newValue) => handleTeamChange('red_team', index, newValue)}
+              options={champions.map(champion => champion.label)}
+              isOptionEqualToValue={(option, value) => (value === '' || option === value)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  placeholder="Select Champion"
+                  sx={{ marginBottom: 1 }}
+                />
+              )}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+              Submit
+          </Button>
+        </Grid>
+      </Grid >
+      </form>
+    </Box>
+  );
 }
 
 export default DraftPredictior
