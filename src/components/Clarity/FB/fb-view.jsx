81c15e7bcbb8a533c82df91a58427ca47107e079
Johnsony0 <johnsony@bu.edu>
2025-08-19 01:11:40 -0400

    starting on the image scroller

--- src/components/Clarity/FB/fb-view.jsx
+++ src/components/Clarity/FB/fb-view.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
+import ScrollReveal from "../ScrollReveal";
+import { Box, Typography } from '@mui/material';
+import fb_before from '../../../assets/clarity/clean_fb_before.png';
+import fb_after from '../../../assets/clarity/clean_fb_after.png';
+
 function ClarityFB(){
   return (
-    <div className="clarity-view">
-        <h1>FB Component</h1>
-        <p>This is the FB view.</p>
-    </div>
+    <>
+    <ScrollReveal
+      beforeImage={fb_before}
+      afterImage={fb_after}
+    />
+    <Box sx={{ height: '2000px', pt: '100vh' }}>
+    </Box>
+  </>
   );
 }
 
